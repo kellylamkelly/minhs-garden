ef6bcbd3579775907c7cf7c135b3fc29c2db890e
Kelly Lam <klam@student.ysu.edu>
2022-10-26 18:47:06 -0400

    created Handlebars Template and home.handlebars

---
+++ Assignment2.js
@@ -0,0 +1,61 @@
+// Include http, express, handlebars, and path
+var http = require("http");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var path = require("path");
+
+// constructs actual express object
+var app = express();
+
+// construct handlebars
+var handlebars = exphbs.create({defaultLayout: 'main'});
+app.engine('.handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static('views'));
+
+// Home page "/"
+app.get("/", function(request, response) {
+    response.render("home");
+});
+
+// Single method for product pages
+app.get("/select/:productID", function(request, response, next) {
+    var langfile = request.params.productID + ".html";
+    response.sendFile(langfile, {root: productPath}, function(err) {
+        if (err) {
+            next(new Error("ProductID file not found"));
+        }
+    });
+});
+
+// First error handling routine logs error
+app.use("/select", function(err, request, response, next) {
+    console.log(err);
+    next(err); // Create new error specific to problem
+});
+
+// Next error handling routine sends back error message (and stops)
+app.use("/select", function(err, request, response, next) {
+    if (err.message.includes("ProductID file not found")) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end('<html><body><h2>Sorry, that product cannot be found!</h2></body></html>');
+    }
+    else {
+        next(err); // if this was not the error, route to the next handler
+    }
+});
+
+// If reach here, request not handled by any previous gets, so send error page
+app.use(function(request, response) {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end('<html><body><h2>Sorry -- file not found!</h2></body></html>');
+});
+
+// If reach here, an unhandled error occurred somewhere previously
+app.use(function(err, request, response, next) {
+    console.log(err);
+    response.writeHead(500, {'Content-Type': 'text/html'});
+    response.end('<html><body><h2>Server error!</h2></body></html>');
+});
+
+http.createServer(app).listen(3000);
