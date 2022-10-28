0b44095ef028c18dc312a3860a6e3d276a8bd694
Kelly Lam <klam@student.ysu.edu>
2022-10-28 01:05:59 -0400

    added images

--- Assignment2.js
+++ Assignment2.js
@@ -1,60 +1,120 @@
 // Include http, express, handlebars, and path
-var http = require("http");
 var express = require("express");
-var exphbs = require("express-handlebars");
+var http = require("http");
 var path = require("path");
+var exphbs = require("express-handlebars");
 
-// constructs actual express object
+// Construct actual express object
 var app = express();
 
-// construct handlebars
-var handlebars = exphbs.create({defaultLayout: 'main'});
+// Set up handlebars
+var handlebars = exphbs.create({ defaultLayout: 'main' });
 app.engine('.handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static('views'));
 
 // Home page "/"
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
     response.render("home");
 });
 
-// Single method for product pages
-app.get("/select/:productID", function(request, response, next) {
-    var langfile = request.params.productID + ".html";
-    response.sendFile(langfile, {root: productPath}, function(err) {
-        if (err) {
-            next(new Error("ProductID file not found"));
+// Product Detail Page. Step 1: Render the list of plant names and their IDs
+app.get("/plants", function (request, response) {
+    const plantList = [
+        {
+            id: "PI100001", name: "Aloe Vera", description: "Aloe vera is a succulent plant species of the genus Aloe. The plant is stemless or very short-stemmed with thick, greenish, fleshy leaves that fan out from the plant's central stem.",
+            price: 5.00, environment: "Indoor", heightEst: "6 to 9 inches", sunExp: "Full, partial", image: "views/images/aloeVera.png"
+        },
+        {
+            id: "PI100002", name: "Spider Plant", description: "The spider plant is a plant in the Chlorophytum genus. It is considered one of the most adaptable of houseplants and the easiest to grow. Spider plants can produce small white flowers.",
+            price: 10.00, environment: "Indoor", heightEst: "8 to 11 inches", sunExp: "Partial, shade", image: "views/images/spiderPlant.png"
+        },
+        {
+            id: "PI100003", name: "Bamboo Palm", description: "A bamboo palm, or Chamaedorea, is a type of palm in the Chamaedorea genus. It's also a rare tropical delight in that, unlike many of its warm-weather cousins, it can actually thrive in lower light.",
+            price: 5.00, environment: "Indoor", heightEst: "6 to 11 inches", sunExp: "Partial, shade", image: "views/images/bambooPalm.png"
         }
-    });
+    ];
+    response.render("plants", {plants: plantList});
 });
 
-// First error handling routine logs error
-app.use("/select", function(err, request, response, next) {
-    console.log(err);
-    next(err); // Create new error specific to problem
+// Product Detail Page. Step 2: Get the requested ID
+app.get("/plants", function (request, response, next) {
+    request.id = request.query.id;
+    next();
 });
 
-// Next error handling routine sends back error message (and stops)
-app.use("/select", function(err, request, response, next) {
-    if (err.message.includes("ProductID file not found")) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end('<html><body><h2>Sorry, that product cannot be found!</h2></body></html>');
+// // Product Detail Page. Step 3: Validate the ID
+// app.use("/plants", function (request, response, next) {
+//     var errors = new Object(); // New (empty) object
+//     var valid = true; // Initially no errors
+
+//     if (request.id.trim() === "") { // Must not be empty
+//         errors.MissingID = true; // Add to error object
+//         valid = false; // Error found
+//     }
+
+//     if (valid) {
+//         next(); // No errors, so continue normal route
+//     }
+//     else {
+//         request.errorList = errors; // At least one error, so add object to request
+//         next(new Error("details")); // and follow error route
+//     }
+// });
+
+// Product Detail Page. Step 4: Display info for requested id
+app.get("/plants", function (request, response, next) {
+    for (let plant of plantList) {
+        if (request.id == plant.id) {
+            var requestedPlant = {id: plant.id,
+                                    name: plant.name,
+                                    description: plant.description,
+                                    price: plant.price,
+                                    environment: plant.environment,
+                                    heightEst: plant.heightEst,
+                                    sunExp: plant.sunExp,
+                                    image: plant.image};
+
+            response.render("plantDetails", {plant: requestedPlant});
+        }
+    }
+});
+
+// If "details" error, redisplay form
+app.use("/plants", function (err, request, response, next) {
+    if (err.message.includes("details")) {
+        const plantList = [
+            {
+                id: "PI100001", name: "Aloe Vera", description: "Aloe vera is a succulent plant species of the genus Aloe. The plant is stemless or very short-stemmed with thick, greenish, fleshy leaves that fan out from the plant's central stem.",
+                price: 5.00, environment: "Indoor", heightEst: "6 to 9 inches", sunExp: "Full, partial", image: "views/images/aloeVera.png"
+            },
+            {
+                id: "PI100002", name: "Spider Plant", description: "The spider plant is a plant in the Chlorophytum genus. It is considered one of the most adaptable of houseplants and the easiest to grow. Spider plants can produce small white flowers.",
+                price: 10.00, environment: "Indoor", heightEst: "8 to 11 inches", sunExp: "Partial, shade", image: "views/images/spiderPlant.png"
+            },
+            {
+                id: "PI100003", name: "Bamboo Palm", description: "A bamboo palm, or Chamaedorea, is a type of palm in the Chamaedorea genus. It's also a rare tropical delight in that, unlike many of its warm-weather cousins, it can actually thrive in lower light.",
+                price: 5.00, environment: "Indoor", heightEst: "6 to 11 inches", sunExp: "Partial, shade", image: "views/images/bambooPalm.png"
+            }
+        ];
+        response.render("plants", {plants: plantList, 
+                                    errors: request.errorList}); // Pass errors to form
     }
     else {
-        next(err); // if this was not the error, route to the next handler
+        next(err); // if not an "details" error, continue error routing
     }
 });
 
 // If reach here, request not handled by any previous gets, so send error page
-app.use(function(request, response) {
-    response.writeHead(404, {'Content-Type': 'text/html'});
+app.use(function (request, response) {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end('<html><body><h2>Sorry -- file not found!</h2></body></html>');
 });
 
 // If reach here, an unhandled error occurred somewhere previously
-app.use(function(err, request, response, next) {
+app.use(function (err, request, response, next) {
     console.log(err);
-    response.writeHead(500, {'Content-Type': 'text/html'});
+    response.writeHead(500, { 'Content-Type': 'text/html' });
     response.end('<html><body><h2>Server error!</h2></body></html>');
 });
 
