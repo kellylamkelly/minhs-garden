a38953f8cd9672023973be745f8a9546a8d21d41
Kelly Lam <2020lamk@gmail.com>
2022-11-11 01:02:35 -0500

    added express-session, implemented cart view, support functions, and logic (not fully completed)

--- Assignment3.js
+++ Assignment3.js
@@ -1,9 +1,10 @@
-// Include http, express, handlebars, and path
+// Include http, express, handlebars, path, and sessions
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var exphbs = require("express-handlebars");
 const res = require("express/lib/response");
+const expressSession = require('express-session');
 
 // Get model functions
 var support = require("./model/model.js");
@@ -11,14 +12,25 @@ var support = require("./model/model.js");
 // Construct actual express object
 var app = express();
 
+// Set session properties
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: "345hjbkSEfjkmdsnfjsdNf98",
+    cookie: {maxAge: 600000} // 600 seconds = 10 mins
+}))
+
 // Set up handlebars
 var handlebars = exphbs.create({ defaultLayout: 'main' });
 app.engine('.handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static('views'));
 
-// Home page "/"
+// Home page "/" - when initially requested, load plant list into session
 app.get("/", function (request, response) {
+    if (!request.session.plantSession) { // if no plant list in session
+        request.session.plantSession = support.getPlants(); // load and store now
+    }
     response.render("home");
 });
 
@@ -36,8 +48,72 @@ app.get("/plantDetails", function (request, response, next) {
 
 // Display info for requested id
 app.get("/plantDetails", function (request, response, next) {
-    var requestedPlant = support.getPlantByID(request.id);
+    var requestedPlant = support.getPlantByID(request.id, request.session.plantSession);
     response.render("plantDetails", {plant: requestedPlant});
+});
+
+// Add plant to cart
+app.get("/add", function(request, response, next) {
+    var id = request.query.id; // get id from request
+    var mycart = request.session.cart; // get cart from session
+    // add plant to cart, getting modified cart as return
+    mycart = support.addPlant(id, mycart, request.session.plantSession);
+    // store modified cart in session
+    request.session.cart = mycart;
+    next();
+});
+
+// Display cart page
+app.get("/cart", function(request, response) {
+    response.render("cart", {cart: request.session.cart,
+                             total: support.getTotal(request.session.cart)});
+});
+
+// Display cart page
+app.get("/add", function(request, response) {
+    response.render("cart", {cart: request.session.cart,
+                             total: support.getTotal(request.session.cart),
+                             addcourse: support.getPlantByID(request.query.id, 
+                                                            request.session.plantSession)});
+});
+
+// Display cart page
+app.get("/remove", function(request, response) {
+    response.render("cart", {cart: request.session.cart,
+                             total: support.getTotal(request.session.cart),
+                             removeplant: support.getPlantByID(request.query.id, 
+                                                            request.session.plantSession)});
+});
+
+// Display cart page
+app.get("/change", function(request, response) {
+    response.render("cart", {cart: request.session.cart,
+                             total: support.getTotal(request.session.cart),
+                             changeplant: support.getPlantByID(request.query.id, 
+                                                            request.session.plantSession)});
+});
+
+// Remove selected plant
+app.get("/remove", function(request, response, next) {
+    var id = request.query.id; // get id from request
+    var mycart = request.session.cart; // get cart from session
+    // call support function to remove plant from cart
+    cart = support.removePlant(id, mycart);
+    // store modified cart back into session
+    request.session.cart = mycart;
+    next();
+});
+
+// Change quantity of selected plant
+app.get("/change", function(request, response, next) {
+    var id = request.query.id; // get id of plant to change
+    var quantity = request.query.quantity; // get new value to change quantity
+    var cart = request.session.cart; // get cart from session
+    // call support function to change quantity for this plant in cart
+    cart = support.changeQuantity(id, quantity, cart);
+    // store modified cart back into session
+    request.session.cart = cart;
+    next();
 });
 
 // If reach here, request not handled by any previous gets, so send error page
--- model/model.js
+++ model/model.js
@@ -1,27 +1,17 @@
-// Search array function to get the object corresponding to ID
-function findPlantByID(array, key, value) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i][key] === value) {
-            return array[i];
-        }
-    }
-    return null;
-}
-
 // Retrieve list of plants
 function getPlants() {
     const plantList = [
         {
             id: "PI100001", name: "Aloe Vera", description: "Aloe vera is a succulent plant species of the genus Aloe. The plant is stemless or very short-stemmed with thick, greenish, fleshy leaves that fan out from the plant's central stem.",
-            price: 5.00, environment: "Indoor", heightEst: "6 to 9 inches", sunExp: "Full, partial", image: "aloeVera.png"
+            price: 5.00, environment: "Indoor", heightEst: "6 to 9 inches", sunExp: "Full, partial", image: "aloeVera.png", quantity: 1, maxQuantity: 5, minQuantity: 1
         },
         {
             id: "PI100002", name: "Spider Plant", description: "The spider plant is a plant in the Chlorophytum genus. It is considered one of the most adaptable of houseplants and the easiest to grow. Spider plants can produce small white flowers.",
-            price: 10.00, environment: "Indoor", heightEst: "8 to 11 inches", sunExp: "Partial, shade", image: "spiderPlant.png"
+            price: 10.00, environment: "Indoor", heightEst: "8 to 11 inches", sunExp: "Partial, shade", image: "spiderPlant.png", quantity: 1, maxQuantity: 5, minQuantity: 1
         },
         {
             id: "PI100003", name: "Bamboo Palm", description: "A bamboo palm, or Chamaedorea, is a type of palm in the Chamaedorea genus. It's also a rare tropical delight in that, unlike many of its warm-weather cousins, it can actually thrive in lower light.",
-            price: 5.00, environment: "Indoor", heightEst: "6 to 11 inches", sunExp: "Partial, shade", image: "bambooPalm.png"
+            price: 5.00, environment: "Indoor", heightEst: "6 to 11 inches", sunExp: "Partial, shade", image: "bambooPalm.png", quantity: 1, maxQuantity: 5, minQuantity: 1
         }
     ];
 
@@ -29,8 +19,7 @@ function getPlants() {
 }
 
 // Search for plant with given id in list
-function getPlantByID(id) {
-    var plantList = getPlants();
+function getPlantByID(id, plantList) {
     for (let plant of plantList) {
         if (id === plant.id) {
             return plant;
@@ -39,17 +28,72 @@ function getPlantByID(id) {
     return null;
 }
 
+// Add plant with given id to cart
+// Parameters: id (from request), cart (stored in session), plants (stored in session)
+function addPlant(id, cart, plantList) {
+    // search list of plants for one with the given id
+    var plant = getPlantByID(id, plantList);
+    
+    // check whether cart exists yet and create if not
+    if (cart == null) {
+        cart = [];
+    }
+
+    cart.push(plant); // add plant to end of cart array
+    return cart;
+}
+
+// Get the index of the plant with the given id in the cart,
+// or -1 if no plant with that id in cart
+function getIndex(id, cart) {
+    count = 0;
+    for (let plant of cart) { // loop through cart array
+        if (id === plant.id) { // compare ids
+            return count; // if match, plant with id at this index
+        }
+        count++;
+    }
+    return -1; // give id not found in cart
+}
+
+// Change quantity of plant with given id to n
+function changeQuantity(id, n, cart) {
+    // get index of plant with given id in cart
+    var where = getIndex(id, cart);
+    // validate this plant is actually in cart
+    if (where != -1) {
+        // change quantity property to new value
+        cart[where].quantity = n;
+    }
+    return cart;
+}
+
+// Remove plants with given id from cart
+function removePlant(id, cart) {
+    // get index of plant with given id in cart
+    var where = getIndex(id, cart);
+    // validate this plant is actually in cart
+    if (where != -1) {
+        // splice list before and list after plant together
+        cart.splice(where, 1);
+    }
+    return cart;
+}
+
 // Compute cost of plant in terms of plant type
-function getPrice(id) {
-    var price = 0;
-    var plant = getPlantByID(id);
-    console.log(plant);
-    price = plant.price;
-    return price;
+function getTotal(cart) {
+    var total = 0; // running total of price
+    for (let plant of cart) {
+        total += parseInt(plant.price);
+    }
+    return total;
 }
 
 // Make function available publicly
-module.exports = {findPlantByID, 
-                    getPlants, 
-                    getPlantByID, 
-                    getPrice}
+module.exports = {getPlants, 
+                    getPlantByID,
+                    getIndex,
+                    changeQuantity,
+                    addPlant,
+                    removePlant,
+                    getTotal}
